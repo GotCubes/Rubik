a8f99da59f20c42652459110f09e6a52570dd721
Spencer Deak <spencerdeak@gmail.com>
2018-06-18 17:33:43 -0400

    Added doubles moveset.

--- rubik.py
+++ rubik.py
@@ -78,6 +78,31 @@ class Cube:
         self.puz[0][3] = self.puz[0][1]
         self.puz[0][1] = temp
 
+    def U2(self):
+        # Rotate adjacent faces twice.
+        temp = [self.puz[1][0], self.puz[1][1], self.puz[1][2]]
+        [self.puz[1][0], self.puz[1][1], self.puz[1][2]] = [self.puz[3][0], self.puz[3][1], self.puz[3][2]]
+        [self.puz[3][0], self.puz[3][1], self.puz[3][2]] = temp
+        temp = [self.puz[4][0], self.puz[4][1], self.puz[4][2]]
+        [self.puz[4][0], self.puz[4][1], self.puz[4][2]] = [self.puz[2][0], self.puz[2][1], self.puz[2][2]]
+        [self.puz[2][0], self.puz[2][1], self.puz[2][2]] = temp
+
+        # Rotate up corners twice.
+        temp = self.puz[0][2]
+        self.puz[0][2] = self.puz[0][6]
+        self.puz[0][6] = temp
+        temp = self.puz[0][8]
+        self.puz[0][8] = self.puz[0][0]
+        self.puz[0][0] = temp
+
+        # Rotate up edges twice.
+        temp = self.puz[0][5]
+        self.puz[0][5] = self.puz[0][3]
+        self.puz[0][3] = temp
+        temp = self.puz[0][7]
+        self.puz[0][7] = self.puz[0][1]
+        self.puz[0][1] = temp
+
     def D(self):
         # Rotate adjacent faces clockwise.
         temp = [self.puz[2][6], self.puz[2][7], self.puz[2][8]]
@@ -120,6 +145,31 @@ class Cube:
         self.puz[5][5] = self.puz[5][7]
         self.puz[5][7] = self.puz[5][3]
         self.puz[5][3] = self.puz[5][1]
+        self.puz[5][1] = temp
+
+    def D2(self):
+        # Rotate adjacent faces twice.
+        temp = [self.puz[3][6], self.puz[3][7], self.puz[3][8]]
+        [self.puz[3][6], self.puz[3][7], self.puz[3][8]] = [self.puz[1][6], self.puz[1][7], self.puz[1][8]]
+        [self.puz[1][6], self.puz[1][7], self.puz[1][8]] = temp
+        temp = [self.puz[4][6], self.puz[4][7], self.puz[4][8]]
+        [self.puz[4][6], self.puz[4][7], self.puz[4][8]] = [self.puz[2][6], self.puz[2][7], self.puz[2][8]]
+        [self.puz[2][6], self.puz[2][7], self.puz[2][8]] = temp
+
+        # Rotate down corners twice.
+        temp = self.puz[5][2]
+        self.puz[5][2] = self.puz[5][6]
+        self.puz[5][6] = temp
+        temp = self.puz[5][8]
+        self.puz[5][8] = self.puz[5][0]
+        self.puz[5][0] = temp
+
+        # Rotate down edges twice.
+        temp = self.puz[5][5]
+        self.puz[5][5] = self.puz[5][3]
+        self.puz[5][3] = temp
+        temp = self.puz[5][7]
+        self.puz[5][7] = self.puz[5][1]
         self.puz[5][1] = temp
 
     def F(self):
@@ -166,6 +216,31 @@ class Cube:
         self.puz[2][3] = self.puz[2][1]
         self.puz[2][1] = temp
 
+    def F2(self):
+        # Rotate adjacent faces twice.
+        temp = [self.puz[3][0], self.puz[3][3], self.puz[3][6]]
+        [self.puz[3][0], self.puz[3][3], self.puz[3][6]] = [self.puz[1][8], self.puz[1][5], self.puz[1][2]]
+        [self.puz[1][8], self.puz[1][5], self.puz[1][2]] = temp
+        temp = [self.puz[5][2], self.puz[5][1], self.puz[5][0]]
+        [self.puz[5][2], self.puz[5][1], self.puz[5][0]] = [self.puz[0][6], self.puz[0][7], self.puz[0][8]]
+        [self.puz[0][6], self.puz[0][7], self.puz[0][8]] = temp
+
+        # Rotate front corners twice.
+        temp = self.puz[2][2]
+        self.puz[2][2] = self.puz[2][6]
+        self.puz[2][6] = temp
+        temp = self.puz[2][8]
+        self.puz[2][8] = self.puz[2][0]
+        self.puz[2][0] = temp
+
+        # Rotate front edges twice.
+        temp = self.puz[2][5]
+        self.puz[2][5] = self.puz[2][3]
+        self.puz[2][3] = temp
+        temp = self.puz[2][7]
+        self.puz[2][7] = self.puz[2][1]
+        self.puz[2][1] = temp
+
     def B(self):
         # Rotate adjacent faces clockwise.
         temp = [self.puz[0][0], self.puz[0][1], self.puz[0][2]]
@@ -208,6 +283,31 @@ class Cube:
         self.puz[4][5] = self.puz[4][7]
         self.puz[4][7] = self.puz[4][3]
         self.puz[4][3] = self.puz[4][1]
+        self.puz[4][1] = temp
+
+    def B2(self):
+        # Rotate adjacent faces twice.
+        temp = [self.puz[1][6], self.puz[1][3], self.puz[1][0]]
+        [self.puz[1][6], self.puz[1][3], self.puz[1][0]] = [self.puz[3][2], self.puz[3][5], self.puz[3][8]]
+        [self.puz[3][2], self.puz[3][5], self.puz[3][8]] = temp
+        temp = [self.puz[5][8], self.puz[5][7], self.puz[5][6]]
+        [self.puz[5][8], self.puz[5][7], self.puz[5][6]] = [self.puz[0][0], self.puz[0][1], self.puz[0][2]]
+        [self.puz[0][0], self.puz[0][1], self.puz[0][2]] = temp
+
+        # Rotate back corners twice.
+        temp = self.puz[4][2]
+        self.puz[4][2] = self.puz[4][6]
+        self.puz[4][6] = temp
+        temp = self.puz[4][8]
+        self.puz[4][8] = self.puz[4][0]
+        self.puz[4][0] = temp
+
+        # Rotate back edges twice.
+        temp = self.puz[4][5]
+        self.puz[4][5] = self.puz[4][3]
+        self.puz[4][3] = temp
+        temp = self.puz[4][7]
+        self.puz[4][7] = self.puz[4][1]
         self.puz[4][1] = temp
 
     def L(self):
@@ -254,6 +354,31 @@ class Cube:
         self.puz[1][3] = self.puz[1][1]
         self.puz[1][1] = temp
 
+    def L2(self):
+        # Rotate adjacent faces twice.
+        temp = [self.puz[5][0], self.puz[5][3], self.puz[5][6]]
+        [self.puz[5][0], self.puz[5][3], self.puz[5][6]] = [self.puz[0][0], self.puz[0][3], self.puz[0][6]]
+        [self.puz[0][0], self.puz[0][3], self.puz[0][6]] = temp
+        temp = [self.puz[4][8], self.puz[4][5], self.puz[4][2]]
+        [self.puz[4][8], self.puz[4][5], self.puz[4][2]] = [self.puz[2][0], self.puz[2][3], self.puz[2][6]]
+        [self.puz[2][0], self.puz[2][3], self.puz[2][6]] = temp
+
+        # Rotate left corners twice.
+        temp = self.puz[1][2]
+        self.puz[1][2] = self.puz[1][6]
+        self.puz[1][6] = temp
+        temp = self.puz[1][8]
+        self.puz[1][8] = self.puz[1][0]
+        self.puz[1][0] = temp
+
+        # Rotate left edges twice.
+        temp = self.puz[1][5]
+        self.puz[1][5] = self.puz[1][3]
+        self.puz[1][3] = temp
+        temp = self.puz[1][7]
+        self.puz[1][7] = self.puz[1][1]
+        self.puz[1][1] = temp
+
     def R(self):
         # Rotate adjacent faces clockwise.
         temp = [self.puz[2][2], self.puz[2][5], self.puz[2][8]]
@@ -298,18 +423,37 @@ class Cube:
         self.puz[3][3] = self.puz[3][1]
         self.puz[3][1] = temp
 
+    def R2(self):
+        # Rotate adjacent faces twice.
+        temp = [self.puz[0][2], self.puz[0][5], self.puz[0][8]]
+        [self.puz[0][2], self.puz[0][5], self.puz[0][8]] = [self.puz[5][2], self.puz[5][5], self.puz[5][8]]
+        [self.puz[5][2], self.puz[5][5], self.puz[5][8]] = temp
+        temp = [self.puz[4][6], self.puz[4][3], self.puz[4][0]]
+        [self.puz[4][6], self.puz[4][3], self.puz[4][0]] = [self.puz[2][2], self.puz[2][5], self.puz[2][8]]
+        [self.puz[2][2], self.puz[2][5], self.puz[2][8]] = temp
+
+        # Rotate right corners twice.
+        temp = self.puz[3][2]
+        self.puz[3][2] = self.puz[3][6]
+        self.puz[3][6] = temp
+        temp = self.puz[3][8]
+        self.puz[3][8] = self.puz[3][0]
+        self.puz[3][0] = temp
+
+        # Rotate right edges twice.
+        temp = self.puz[3][5]
+        self.puz[3][5] = self.puz[3][3]
+        self.puz[3][3] = temp
+        temp = self.puz[3][7]
+        self.puz[3][7] = self.puz[3][1]
+        self.puz[3][1] = temp
+
 if __name__ == "__main__":
     cube = Cube()
-    cube.Up()
-    cube.Up()
-    cube.Dp()
-    cube.Dp()
-    cube.Lp()
-    cube.Lp()
-    cube.Rp()
-    cube.Rp()
-    cube.Fp()
-    cube.Fp()
-    cube.Bp()
-    cube.Bp()
+    cube.U2()
+    cube.D2()
+    cube.L2()
+    cube.R2()
+    cube.F2()
+    cube.B2()
     print(cube)
