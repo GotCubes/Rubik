ec8d98c986bc428c6ebd27c13075048fb34c0515
Spencer Deak <spencerdeak@gmail.com>
2018-06-19 13:02:40 -0400

    Added string representation.

--- rubik.py
+++ rubik.py
@@ -5,13 +5,23 @@ from moves import *
 class Cube:
     def __init__(self):
         self.puz = np.chararray((9, 12))
-        self.puz[:] = '_'
+        self.puz[:] = "_"
         self.puz[0:3, 3:6] = 'W'
         self.puz[3:6, 0:3] = 'O'
         self.puz[3:6, 3:6] = 'G'
         self.puz[3:6, 6:9] = 'R'
         self.puz[3:6, 9:12] = 'B'
         self.puz[6:9, 3:6] = 'Y'
+
+    def __str__(self):
+        ret = ""
+        for line in self.puz:
+            for item in line:
+                if item != "_": ret += item + " "
+                else: ret += "  "
+            ret += "\n"
+
+        return ret
 
     def execute(self, algorithm):
         for move in algorithm:
@@ -23,6 +33,7 @@ if __name__ == "__main__":
     superflip = [U, R2, F, B, R, B2, R, U2, L, B2, R, Up, Dp, R2, F, Rp, L, B2, U2, F2]
 
     cube.execute(superflip)
-    print(cube.puz)
+    cube.execute(checkerboard)
     cube.execute(superflip)
-    print(cube.puz)
+    cube.execute(checkerboard)
+    print(cube)
